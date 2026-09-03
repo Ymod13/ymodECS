97584b85631345b2eec1377525eda2df751684de
Ymod13 <136576607+Ymod13@users.noreply.github.com>
2026-09-03 11:17:24 +0300

    added Dist dir for distribution

---
+++ dist/Scripts/Config.lua
@@ -0,0 +1,23 @@
+-- config.lua
+
+-- WINDOW
+window_title = "Ympd ECS prototype"
+screen_width = 2200
+screen_height = 1200
+map_size =  Vector2D.new(5000.0, 5000.0)
+camera_pos =  Vector2D.new(1000.0, 1000.0)
+is_fullscreen = false
+
+-- FOLDERS
+sprites_folder = "Sprites/";
+
+-- DEBUG
+display_lua_debug_messages = true;
+is_text_debug = false;
+is_input_text_debug = false;
+display_stats = true;
+stats_display_interval = 0.5;
+
+-- COLLISIONS
+cell_size = 32;
+alpha_threshold = 20;
